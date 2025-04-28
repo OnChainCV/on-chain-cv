17f82e4115966cbd2053e6c39d22fb45646ca5ad
OnChainCV <onchaincv@gmail.com>
2025-04-28 15:47:18 +0300

    Add connect solana wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
+import WalletButton from "@/components/Solana/WalletButton";
 
-export default function Home() {
-  const isWalletConnected = true;
+
+export default function Home() {  
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
@@ -9,23 +10,9 @@ export default function Home() {
         <h1 className="text-4xl font-extrabold text-gray-800 mb-6">
           Web3 Профіль в майбутньому
         </h1>
-
         <div className="mb-8">
-          {isWalletConnected ? (
-            <div className="text-lg text-green-600 mb-4">
-              <span>Гаманець підключено!</span>
-            </div>
-          ) : (
-            <div className="text-lg text-red-600 mb-4">
-              <span>Гаманець не підключено</span>
-            </div>
-          )}
-
-          <button className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition duration-300 mb-4">
-            {isWalletConnected ? 'Підключений' : 'Підключити гаманець'}
-          </button>
+          <WalletButton />
         </div>
-
         <Link href="/edit">
 
           <button className="w-full bg-indigo-600 text-white py-3 rounded-md hover:bg-indigo-700 transition duration-300">
